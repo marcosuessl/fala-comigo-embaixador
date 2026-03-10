df8205386b6038870f3b11aa56c00e179f29bcf1
marcosuessl <uesslerjunior@gmail.com>
2026-03-10 19:21:46 -0300

    Add files via upload

---
+++ projeto-gusttavo-lima/script.js
@@ -0,0 +1,20 @@
+const foto = document.getElementById('btn-gusttavo');
+
+// Essa lista tem que ter os nomes EXATOS dos arquivos que aparecem na sua esquerda
+const frases = [
+    'audio1.mp3',
+    'audio2.mp3',
+    'audio3.mp3',
+    'audio4.mp3',
+    'gustavo-lima-i-vc.mp3'
+];
+
+foto.addEventListener('click', () => {
+    // Sorteia uma posição da lista (0, 1, 2, 3 ou 4)
+    const indiceAleatorio = Math.floor(Math.random() * frases.length);
+    const somSorteado = frases[indiceAleatorio];
+
+    // Toca o som que foi sorteado
+    const som = new Audio(somSorteado);
+    som.play();
+});
